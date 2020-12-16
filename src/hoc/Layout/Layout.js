fa4036d4d1618d6686b45830d227827531911f6b
KappRoss <kaplicnyj777@gmail.com>
2020-12-16 15:54:01 +0200

    Part 1 Finished

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Drawer from '../../components/Navigation/Drawer/Drawer';
 import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle';
 import classes from './Layout.module.css';
 
@@ -13,10 +14,19 @@ class Layout extends React.Component{
       menu: !this.state.menu
     })
   }
-
+  menuCloseHandler = () => {
+    this.setState({
+      menu: false
+    })
+  }
   render(){
     return (
       <div className = {classes.Layout}>
+
+        <Drawer
+          isOpen = {this.state.menu}
+          onClose = {this.menuCloseHandler}
+        />
 
         <MenuToggle 
           onToggle = {this.toggleMenuHandler}
